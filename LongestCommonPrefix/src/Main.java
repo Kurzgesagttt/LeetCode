6efd035c5aa605333fcd6e7945db3d5cd0f38831
Lucas <lucasmorenorodrigues@hotmail.com>
2025-07-31 12:00:55 -0300

    LongestComminPrefix

--- LongestCommonPrefix/src/Main.java
+++ LongestCommonPrefix/src/Main.java
@@ -2,26 +2,25 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static String longestCommonPrefix(String[] strs) {
-        String prefix = "";
-            for(int i = 0 ; i < strs.length; i++){
-                char[] charArray = strs[i].toCharArray();
-                for (int x = 0; x < strs[i].length() ; x++){
-                    char letter = charArray[x];
-                    System.out.println(letter);
+        if (strs == null || strs.length == 0) return "";
+        StringBuilder stringBuilder = new StringBuilder();
 
+            for(int i = 0 ; i < strs[0].length(); i++){
+                char c = strs[0].charAt(i);
+
+                for (int j = 1; j < strs.length; j++){
+                    if( i >= strs[j].length() || strs[j].charAt(i) != c ){
+                        return stringBuilder.toString();
+                    }
                 }
-
-
-
+                stringBuilder.append(c);
             }
-        return "";
+        return stringBuilder.toString();
     }
 
     public static void main(String[] args) {
-        String[] strs = new String[] {"flower","flow","flight"};
-
+        String[] strs = new String[] {"flower","flower","flower","flower"};
         String result = longestCommonPrefix(strs);
-
         System.out.println(result);
     }
 }
